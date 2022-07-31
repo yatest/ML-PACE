81781353c308812d8624f0c4bf69b799f08365f6
Tom Yates <tomyates1996@hotmail.co.uk>
2022-07-31 02:15:58 +0100

    debugging

--- pair_pace.cpp
+++ pair_pace.cpp
@@ -152,7 +152,6 @@ void PairPACE::compute(int eflag, int vflag) {
     int *ilist, *jlist, *numneigh, **firstneigh;
 
     ev_init(eflag, vflag);
-    fprintf(screen,"Checkpoint 1 reached.\n");
     // downwards modified by YL
 
     double **x = atom->x;
@@ -186,7 +185,6 @@ void PairPACE::compute(int eflag, int vflag) {
         error->all(FLERR, str);
     }
 
-    fprintf(screen,"Checkpoint 2 reached.\n");
     // Aidan Thompson told RD (26 July 2019) that practically always holds:
     // inum = nlocal
     // i = ilist(ii) < inum
@@ -214,7 +212,6 @@ void PairPACE::compute(int eflag, int vflag) {
             ace_list[k]->resize_neighbours_cache(max_jnum);   
         }
     }
-    fprintf(screen,"Checkpoint 3 reached.\n");
     //loop over atoms
     for (ii = 0; ii < list->inum; ii++) {
         i = list->ilist[ii];
@@ -254,7 +251,6 @@ void PairPACE::compute(int eflag, int vflag) {
                 }
             }
         }
-        fprintf(screen,"Checkpoint 4 reached.\n");
         // 'compute_atom' will update the `ace->e_atom` and `ace->neighbours_forces(jj, alpha)` arrays
 
         for (jj = 0; jj < jnum; jj++) {
@@ -302,7 +298,6 @@ void PairPACE::compute(int eflag, int vflag) {
                              fij[0], fij[1], fij[2],
                              -delx, -dely, -delz);
         }
-        fprintf(screen,"Checkpoint 5 reached.\n");
         // tally energy contribution
         if (eflag) {
             // evdwl = energy of atom I
@@ -313,7 +308,6 @@ void PairPACE::compute(int eflag, int vflag) {
             }
             ev_tally_full(i, 2.0 * evdwl, 0.0, 0.0, 0.0, 0.0, 0.0);
         }            
-        fprintf(screen,"Checkpoint 6 reached.\n");
     }
 
     if (vflag_fdotr) virial_fdotr_compute();
@@ -727,11 +721,9 @@ double PairPACE::init_one(int i, int j) {
         // all procs != 0 do not currently have the basis functions etc. loaded yet
         // maybe MPI_Reduce or such call?
         if (comm->me == 0) {
-        fprintf(screen,"init_one checkpoint");
-        fprintf(screen,"crad[0] = %f", basis_set_list[0]->radial_functions->crad(0, 0, 0, 0, 0));
-        /* TWY: currently assuming cutoff (and radial functions) are the same in
-                all interpolation potentials. This may not always be true, though. */
-        return basis_set_list[0]->radial_functions->cut(map[i], map[j]);
+            /* TWY: currently assuming cutoff (and radial functions) are the same in
+                    all interpolation potentials. This may not always be true, though. */
+            return basis_set_list[0]->radial_functions->cut(map[i], map[j]);
         }
     }
 }
