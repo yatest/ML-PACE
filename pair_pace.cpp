4e2953b0bb9df12a2001adc6ddfc92d443987519
Tom Yates <tomyates1996@hotmail.co.uk>
2022-08-01 22:54:59 +0100

    debugging MPI

--- pair_pace.cpp
+++ pair_pace.cpp
@@ -518,6 +518,10 @@ void PairPACE::coeff(int narg, char **arg) {
         }
         MPI_Bcast(&temps_list[0],temps_list.size(),MPI_INT,0,world);
         MPI_Bcast(&potential_file_name_list[0],potential_file_name_list.size()*sizeof(decltype(potential_file_name_list)::value_type),MPI_BYTE,0,world);
+        if (comm->me != 0) {
+            fprintf(screen, "temps_list[0] = %d", temps_list[0]);
+            fprintf(screen, "potential_file_name_list[0] = %s", potential_file_name_list[0]);
+        }
 
         // TWY: load all potential files if interpolating
         // access individual objects as basis_set_list[i]
