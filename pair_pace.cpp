27f8d933c1547d2d83162f02c3503e5761a88dd2
Tom Yates <tomyates1996@hotmail.co.uk>
2022-08-03 15:35:32 +0100

    debugging MPI

--- pair_pace.cpp
+++ pair_pace.cpp
@@ -525,7 +525,6 @@ void PairPACE::coeff(int narg, char **arg) {
             MPI_Bcast(&pot_file_len,1,MPI_INT,0,world);
             if (comm->me == 0) fprintf(screen, "proc 0 pot_file_len = %d\n", pot_file_len);
             if (comm->me != 0) fprintf(screen, "proc %d pot_file_len = %d\n", comm->me, pot_file_len);
-            fprintf("")
             MPI_Bcast(*potential_file_name_list[i],pot_file_len,MPI_BYTE,0,world);
         }
 
